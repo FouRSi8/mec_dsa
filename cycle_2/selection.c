3c5da49cec86723f50be4b52e3731d42ff99a37d
FouRSi8 <148006771+FouRSi8@users.noreply.github.com>
2023-10-25 15:30:06 +0530

    Add files via upload

---
+++ cycle_2/selection.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+void selectionsort(int a[],int n)
+{
+int i,j,temp,min_index;
+for(i=0;i<n-1;i++)  // finding smallest element and placing in position
+    { min_index=i;
+        for(j=i+1;j<n;j++)
+        {
+            if(a[j]<a[min_index])
+            {
+            min_index=j;
+            }
+        }
+        if(min_index!=i) { temp=a[i];a[i]=a[min_index];a[min_index]=temp;}
+ }
+}
+
+int main(){
+int size,i;
+
+printf("Enter size of the array: ");
+scanf("%d",&size);
+
+int a[size];
+
+printf("Enter %d elements: ",size);
+for(i=0;i<size;i++)
+scanf("%d",&a[i]);
+
+selectionsort(a,size);
+
+printf("Sorted elements: ");
+for(i=0;i<size;i++)
+printf(" %d",a[i]);
+
+return 0;
+}
